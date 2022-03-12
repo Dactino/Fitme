b94e8eda53641bf17689e6e7d59bb46ef9434d70
Dactino <daniel.marmar@educa.jcyl.es>
2022-03-12 21:29:50 +0100

    Creación de ejercicios "automáticos" hecha
    Ya se pueden conseguir las claves de la base de datos

--- src/components/newEjercicio.vue
+++ src/components/newEjercicio.vue
@@ -5,16 +5,13 @@
     Repeticiones Ejercicio:
     <input type="text" v-model="newEjercicio.repeticiones" />
     Nombre categoria:
-    <input
-      type="text"
-      v-model="newEjercicio.categoria"
-    />
+    <input type="text" v-model="newEjercicio.categoria" />
     <input type="submit" value="Enviar" />
   </form>
 </template>
 
 <script>
-import { ref, push, get } from "firebase/database";
+import { ref, push, onValue } from "firebase/database";
 import db from "./bd/bd_config.js";
 
 export default {
@@ -28,12 +25,12 @@ export default {
         categoria: "",
         imagen: "",
       },
-      categoriasRef: get(ref(db, "Categorias_ejercicios")),
+      categoriasRef: ref(db, "Categorias_ejercicios"),
     };
   },
   methods: {
     addEjercicio() {
-      this.newEjercicio.imagen = this.newEjercicio.nombre + ".jpg"
+      this.newEjercicio.imagen = this.newEjercicio.nombre + ".jpg";
 
       //con set() se eliminan las anteriores entradas, mientras que con update se modifican, y con push se añaden
       push(ref(db, "Ejercicios"), {
@@ -47,24 +44,20 @@ export default {
 
     //Con la siguiente función se intenta conseguir el id de la categoría cuyo nombre coincida con el proporcionado
     buscarCategoria() {
-      this.categoriasRef
-        .then((categorias) => {
-          if (categorias.exists()) {
-            console.log(categorias.val());
-            categorias.val().forEach((categoria) => {
-            if (categoria.nombre == this.newEjercicio.categoria) {
+      onValue(
+        this.categoriasRef,
+        (categorias) => {
+          categorias.forEach((categoria) => {
+            if (this.newEjercicio.categoria == categoria.val().nombre) {
               this.newEjercicio.categoria = categoria.key;
-              return;
+              this.addEjercicio();
             }
           });
-          //this.addEjercicio();
-          } else {
-            console.log("No data available");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+        },
+        {
+          onlyOnce: true,
+        }
+      );
     },
   },
 };
